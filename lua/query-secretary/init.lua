29fd2a686beb5e44813bb8c67280e3198707e4b4
ziontee113 <ziontee113@gmail.com>
2022-10-23 18:22:29 +0700

    add(module): lib/window.lua

--- lua/query-secretary/init.lua
+++ lua/query-secretary/init.lua
@@ -1,4 +1,47 @@
+---@diagnostic disable: undefined-global, need-check-nil
 local M = {}
+local ts = require("query-secretary.lib.tree-sitter")
+local window = require("query-secretary.lib.window")
+
+local ns = vim.api.nvim_create_namespace("query-secretary")
+vim.api.nvim_buf_clear_namespace(0, ns, 0, -1)
+
+local function extmark_self_and_parents()
+	local parents = ts.get_parent_nodes_at_cursor({ include_current_node = true })
+	local result_query = ""
+
+	for i = #parents, 1, -1 do
+		local new_line = i > 1 and "\n" or ""
+
+		local content = parents[i]:type()
+		content = string.rep("\t", #parents - i) .. "(" .. content
+
+		result_query = result_query .. content .. new_line
+		if i == 1 then
+			result_query = result_query .. string.rep(")", #parents)
+		end
+	end
+
+	N(result_query)
+end
+
+vim.keymap.set("n", "<C-x><C-x>", function()
+	vim.api.nvim_buf_set_extmark(0, ns, 0, 0, {
+		virt_text = { { "a", "Normal" } },
+		virt_text_pos = "overlay",
+	})
+end, {})
+
+vim.keymap.set("n", "<C-x><C-c>", function()
+	vim.api.nvim_buf_clear_namespace(0, ns, 0, -1)
+end, {})
+
+vim.keymap.set("n", "<C-x><C-p>", function()
+	window.open_center_window({ width = 24, height = 10 })
+end, {})
+
+vim.keymap.set({ "n", "x" }, "<C-x><C-n>", function()
+	extmark_self_and_parents()
+end, {})
 
 return M
--- {{{nvim-execute-on-save}}}
---
+++ lua/query-secretary/lib/window.lua
@@ -0,0 +1,45 @@
+local M = {}
+
+---open floating window at the center of the editor
+---@param open_win_opts table
+---@param win_set_opts table | nil
+M.open_center_window = function(open_win_opts, win_set_opts)
+	local buf, win
+
+	-- buf options
+	buf = vim.api.nvim_create_buf(false, true)
+	vim.api.nvim_buf_set_option(buf, "filetype", "query")
+	vim.api.nvim_buf_set_option(buf, "bufhidden", "delete")
+
+	-- get editorStats
+	local editorStats = vim.api.nvim_list_uis()[1]
+	local editorWidth = editorStats.width
+	local editorHeight = editorStats.height
+
+	-- handle open_win_opts
+	local default_opts = {
+		relative = "editor",
+		col = math.ceil((editorWidth - open_win_opts.width) / 2),
+		row = math.ceil((editorHeight - open_win_opts.height) / 2) - 1,
+		style = "minimal",
+		border = "single",
+		width = 24,
+		height = 10,
+	}
+	open_win_opts = vim.tbl_extend("force", default_opts, open_win_opts)
+
+	-- open window with open_win_opts
+	win = vim.api.nvim_open_win(buf, true, open_win_opts)
+
+	-- handle win_set_opts
+	local default_win_set_opts = {
+		winhl = "NormalFloat:",
+		cursorline = true,
+	}
+	win_set_opts = vim.tbl_extend("force", default_win_set_opts, win_set_opts or {})
+	for key, value in pairs(win_set_opts) do
+		vim.api.nvim_win_set_option(win, key, value)
+	end
+end
+
+return M
