f03b450619aea994b301ce56e65593ce7a1ef219
ziontee113 <ziontee113@gmail.com>
2022-10-24 19:21:35 +0700

    feat(lua-utils): add increment_index_or_index_1

--- lua/query-secretary/lib/lua-utils.lua
+++ lua/query-secretary/lib/lua-utils.lua
@@ -1,10 +1,28 @@
 local M = {}
 
+---returns `index` of *value* in *tbl*
+---@param tbl table
+---@param value any
+---@return integer|nil
 M.tbl_index_of = function(tbl, value)
 	for i, v in ipairs(tbl) do
 		if v == value then
 			return i
 		end
+	end
+end
+
+---if *index + increment <= #tbl*, returns `index + increment`
+---otherwise returns `1`
+---@param tbl table
+---@param index integer
+---@param increment integer
+---@return number
+M.increment_index_or_index_1 = function(tbl, index, increment)
+	if index + increment <= #tbl then
+		return index + increment
+	else
+		return 1
 	end
 end
 
